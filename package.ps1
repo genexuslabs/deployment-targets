92c9c8fe5addc487cfaf6f781a442a405ca67913
Seba Gómez <sgomez@genexus.com>
2020-06-15 16:56:17 -0300

    Fixed package version

--- package.ps1
+++ package.ps1
@@ -1,1 +1,11 @@
-nuget pack .\GeneXus.DeploymentTargets.nuspec
+$nuspec = ".\GeneXus.DeploymentTargets.nuspec"
+
+$Timestamp = (Get-Date).ToString("yyyyMMddHHmmss")
+
+$xml = Get-Content $nuspec -Raw
+$xml = $xml -replace "trunk-\*","trunk-$Timestamp"
+
+Set-Content $nuspec $xml
+
+
+nuget pack .\GeneXus.DeploymentTargets.nuspec 
