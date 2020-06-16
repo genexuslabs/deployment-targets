f30a04509b3f61a9fb8897692ecf937f4e3e137c
Seba Gómez <sgomez@genexus.com>
2020-06-16 17:09:38 -0300

    Added needed jars for AWSServerless deployment Include a nuspec template instead of the original file Ignore generated nuspec and nupkg files Fixed typo in package folder (DeploymentTargets, plural)

--- package.ps1
+++ package.ps1
@@ -1,8 +1,9 @@
+$template = ".\GeneXus.DeploymentTargets.nuspec.template"
 $nuspec = ".\GeneXus.DeploymentTargets.nuspec"
 
 $Timestamp = (Get-Date).ToString("yyyyMMddHHmmss")
 
-$xml = Get-Content $nuspec -Raw
+$xml = Get-Content $template -Raw
 $xml = $xml -replace "trunk-\*","trunk-$Timestamp"
 
 Set-Content $nuspec $xml
