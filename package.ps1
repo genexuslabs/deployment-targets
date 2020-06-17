844520f9599d2051a44597f5e0d4b0312995745c
Seba Gómez <sgomez@genexus.com>
2020-06-16 22:40:43 -0300

    Fixed version template

--- package.ps1
+++ package.ps1
@@ -4,7 +4,7 @@ $nuspec = ".\GeneXus.DeploymentTargets.nuspec"
 $Timestamp = (Get-Date).ToString("yyyyMMddHHmmss")
 
 $xml = Get-Content $template -Raw
-$xml = $xml -replace "trunk-\*","trunk-$Timestamp"
+$xml = $xml -replace "trunk.\*","trunk.$Timestamp"
 
 Set-Content $nuspec $xml
 
