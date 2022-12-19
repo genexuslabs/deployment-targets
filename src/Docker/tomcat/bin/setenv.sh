0137643db31a540cda5e0bd6962efacaca54f129
Gonzalo <gonzalogallotti@gmail.com>
2022-12-19 11:53:54 -0300

    Cherry pick branch 'genexuslabs:remove-useconcmarksweeper' into beta

--- src/Docker/tomcat/bin/setenv.sh
+++ src/Docker/tomcat/bin/setenv.sh
@@ -1,6 +1,5 @@
 echo "GeneXus - Setting custom environment variables for Tomcat"
 
-export CATALINA_OPTS="$CATALINA_OPTS -XX:+UseConcMarkSweepGC"
 export CATALINA_OPTS="$CATALINA_OPTS -XX:HeapDumpPath=/logs/tomcat/java_heapdump_pid_%p.log"
 export CATALINA_OPTS="$CATALINA_OPTS -XX:+HeapDumpOnOutOfMemoryError"
 export CATALINA_OPTS="$CATALINA_OPTS -Djava.awt.headless=true"
