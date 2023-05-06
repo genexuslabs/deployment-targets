5bb23bda44a4460af5fd21414e4c801c83202d90
sjuarezgx <sjuarez@genexus.com>
2023-05-06 09:40:38 -0300

    Support Azure App Insights OpenTelemetry (#74)
    * Support Azure App Insights OpenTelemetry Java

---
+++ src/Docker/scripts/otelenv_AzureMonitor.sh
@@ -0,0 +1,5 @@
+#OpenTelemetry Configuration
+
+echo "GeneXus - Setting OpenTelemetry environment variables for Tomcat"
+
+export CATALINA_OPTS="$CATALINA_OPTS -javaagent:/usr/local/tomcat/lib/applicationinsights-agent.jar"
